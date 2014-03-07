998b4ec4122d47bcb9f912f063428fb76ed71634
unknown <the3dguy@.(none)>
2014-03-06 22:22:08 -0500

    - Fixed the checking of wins to display a win properly - Added AI logic for when human moves to the middle first

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,7 +18,7 @@ angular.module('ticTacToeApp').controller('MainCtrl', ['$scope', function ($scop
           //Sets the actual move for the human player
           $scope.squares[index].val = 'X';
           moves.push(index);
-          //Checks if the player won the game
+          //Checks if the AI won the game
           checkIfWon();
           //Computer will move
           aiMove();
@@ -27,64 +27,53 @@ angular.module('ticTacToeApp').controller('MainCtrl', ['$scope', function ($scop
     
     //This function will check all 8 possible combinations of winning and report a winner
     function checkIfWon() {
-        //Initial outer check prevents as much logic needing to be checked
-        if($scope.squares[0].val) {
-          //Check top line win
-          if($scope.squares[0].val === $scope.squares[1].val && $scope.squares[0].val === $scope.squares[2].val) {
-            console.log('"'+$scope.squares[0].val + '" wins on top horizontal line');
-            $scope.squares[0].win = $scope.squares[1].win = $scope.squares[2].win = true;
-            return;
-          }
-          //Check diagnol top left to bottom right win
-          else if($scope.squares[0].val === $scope.squares[4].val && $scope.squares[0].val === $scope.squares[8].val) {
-            console.log('"'+$scope.squares[0].val + '" wins on diagnol down right');
-            $scope.squares[0].win = $scope.squares[4].win = $scope.squares[8].win = true;
-            return;
-          }
-          //Check left vertical line win
-          else if($scope.squares[0].val === $scope.squares[3].val && $scope.squares[0].val === $scope.squares[6].val) {
-            console.log('"'+$scope.squares[0].val + '" wins on left vertical line');
-            $scope.squares[0].win = $scope.squares[3].win = $scope.squares[6].win = true;
-            return;
-          }
+        //Check top line win
+        if($scope.squares[0].val && $scope.squares[0].val === $scope.squares[1].val && $scope.squares[0].val === $scope.squares[2].val) {
+          console.log('"'+$scope.squares[0].val + '" wins on top horizontal line');
+          $scope.squares[0].win = $scope.squares[1].win = $scope.squares[2].win = true;
+          return;
         }
-        else if($scope.squares[1].val) {
-          //Check middle vertical line win
-          if($scope.squares[1].val === $scope.squares[4].val && $scope.squares[1].val === $scope.squares[7].val) {
-            console.log('"'+$scope.squares[1].val + '" wins on  middle vertical line');
-            $scope.squares[1].win = $scope.squares[4].win = $scope.squares[7].win = true;
-            return;
-          }
+        //Check diagnol top left to bottom right win
+        else if($scope.squares[0].val && $scope.squares[0].val === $scope.squares[4].val && $scope.squares[0].val === $scope.squares[8].val) {
+          console.log('"'+$scope.squares[0].val + '" wins on diagnol down right');
+          $scope.squares[0].win = $scope.squares[4].win = $scope.squares[8].win = true;
+          return;
         }
-        else if($scope.squares[2].val) {
-          //Check right vertical line win
-          if($scope.squares[2].val === $scope.squares[5].val && $scope.squares[2].val === $scope.squares[8].val) {
-            console.log('"'+$scope.squares[2].val + '" wins on  right vertical line');
-            $scope.squares[2].win = $scope.squares[5].win = $scope.squares[8].win = true;
-            return;
-          }
-          //Check diagnol bottom left to top right win
-          else if($scope.squares[2].val === $scope.squares[4].val && $scope.squares[2].val === $scope.squares[6].val) {
-            console.log('"'+$scope.squares[2].val + '" wins on diagnol up right');
-            $scope.squares[2].win = $scope.squares[4].win = $scope.squares[6].win = true;
-            return;
-          }
+        //Check left vertical line win
+        else if($scope.squares[0].val && $scope.squares[0].val === $scope.squares[3].val && $scope.squares[0].val === $scope.squares[6].val) {
+          console.log('"'+$scope.squares[0].val + '" wins on left vertical line');
+          $scope.squares[0].win = $scope.squares[3].win = $scope.squares[6].win = true;
+          return;
         }
-        else if($scope.squares[3].val) {
-          //Check middle horizontal line win
-          if($scope.squares[3].val === $scope.squares[4].val && $scope.squares[3].val === $scope.squares[5].val) {
-            console.log('"'+$scope.squares[3].val + '" wins on middle horizontal line');
-            $scope.squares[3].win = $scope.squares[4].win = $scope.squares[5].win = true;
-            return;
-          }
+        //Check middle vertical line win
+        else if($scope.squares[1].val && $scope.squares[1].val === $scope.squares[4].val && $scope.squares[1].val === $scope.squares[7].val) {
+          console.log('"'+$scope.squares[1].val + '" wins on  middle vertical line');
+          $scope.squares[1].win = $scope.squares[4].win = $scope.squares[7].win = true;
+          return;
         }
-        else if($scope.squares[6].val) {
-          //Check bottom horizontal line win
-          if($scope.squares[6].val === $scope.squares[7].val && $scope.squares[6].val === $scope.squares[8].val) {
-            console.log('"'+$scope.squares[6].val + '" wins on bottom horizontal line');
-            $scope.squares[6].win = $scope.squares[7].win = $scope.squares[8].win = true;
-            return;
-          }
+        //Check right vertical line win
+        else if($scope.squares[2].val && $scope.squares[2].val === $scope.squares[5].val && $scope.squares[2].val === $scope.squares[8].val) {
+          console.log('"'+$scope.squares[2].val + '" wins on  right vertical line');
+          $scope.squares[2].win = $scope.squares[5].win = $scope.squares[8].win = true;
+          return;
+        }
+        //Check diagnol bottom left to top right win
+        else if($scope.squares[2].val && $scope.squares[2].val === $scope.squares[4].val && $scope.squares[2].val === $scope.squares[6].val) {
+          console.log('"'+$scope.squares[2].val + '" wins on diagnol up right');
+          $scope.squares[2].win = $scope.squares[4].win = $scope.squares[6].win = true;
+          return;
+        }
+        //Check middle horizontal line win
+        else if($scope.squares[3].val && $scope.squares[3].val === $scope.squares[4].val && $scope.squares[3].val === $scope.squares[5].val) {
+          console.log('"'+$scope.squares[3].val + '" wins on middle horizontal line');
+          $scope.squares[3].win = $scope.squares[4].win = $scope.squares[5].win = true;
+          return;
+        }
+        //Check bottom horizontal line win
+        else if($scope.squares[6].val && $scope.squares[6].val === $scope.squares[7].val && $scope.squares[6].val === $scope.squares[8].val) {
+          console.log('"'+$scope.squares[6].val + '" wins on bottom horizontal line');
+          $scope.squares[6].win = $scope.squares[7].win = $scope.squares[8].win = true;
+          return;
         }
       }
     
@@ -143,7 +132,69 @@ angular.module('ticTacToeApp').controller('MainCtrl', ['$scope', function ($scop
               }
               //Move was blocked, move to next available space
               else {
-                aiChooseAvailSpace();
+                aiChooseAvailSpaceMiddleFirst();
+              }
+            }
+            //If Players first move was the middle, now block or win
+            else {
+              if($scope.squares[0].val === 'X' && $scope.squares[1].val === 'X' && !$scope.squares[2].val) {
+                aiMoveSet(2);
+              }
+              else if($scope.squares[0].val === 'X' && $scope.squares[2].val === 'X' && !$scope.squares[1].val) {
+                aiMoveSet(1);
+              }
+              else if($scope.squares[1].val === 'X' && $scope.squares[2].val === 'X' && !$scope.squares[0].val) {
+                aiMoveSet(0);
+              }
+              else if($scope.squares[0].val === 'X' && $scope.squares[3].val === 'X' && !$scope.squares[6].val) {
+                aiMoveSet(6);
+              }
+              else if($scope.squares[0].val === 'X' && $scope.squares[6].val === 'X' && !$scope.squares[3].val) {
+                aiMoveSet(3);
+              }
+              else if($scope.squares[3].val === 'X' && $scope.squares[6].val === 'X' && !$scope.squares[0].val) {
+                aiMoveSet(0);
+              }
+              else if($scope.squares[2].val === 'X' && $scope.squares[5].val === 'X' && !$scope.squares[8].val) {
+                aiMoveSet(8);
+              }
+              else if($scope.squares[2].val === 'X' && $scope.squares[8].val === 'X' && !$scope.squares[5].val) {
+                aiMoveSet(5);
+              }
+              else if($scope.squares[5].val === 'X' && $scope.squares[8].val === 'X' && !$scope.squares[2].val) {
+                aiMoveSet(2);
+              }
+              else if($scope.squares[6].val === 'X' && $scope.squares[7].val === 'X' && !$scope.squares[8].val) {
+                aiMoveSet(8);
+              }
+              else if($scope.squares[7].val === 'X' && $scope.squares[8].val === 'X' && !$scope.squares[6].val) {
+                aiMoveSet(6);
+              }
+              else if($scope.squares[6].val === 'X' && $scope.squares[8].val === 'X' && !$scope.squares[7].val) {
+                aiMoveSet(7);
+              }
+              //Account for the middle space also since the user has moved into that space
+              else if($scope.squares[0].val === 'X' && $scope.squares[4].val === 'X' && !$scope.squares[8].val) {
+                aiMoveSet(8);
+              }
+              else if($scope.squares[8].val === 'X' && $scope.squares[4].val === 'X' && !$scope.squares[0].val) {
+                aiMoveSet(0);
+              }
+              else if($scope.squares[3].val === 'X' && $scope.squares[4].val === 'X' && !$scope.squares[5].val) {
+                aiMoveSet(5);
+              }
+              else if($scope.squares[5].val === 'X' && $scope.squares[4].val === 'X' && !$scope.squares[3].val) {
+                aiMoveSet(3);
+              }
+              else if($scope.squares[1].val === 'X' && $scope.squares[4].val === 'X' && !$scope.squares[7].val) {
+                aiMoveSet(7);
+              }
+              else if($scope.squares[7].val === 'X' && $scope.squares[4].val === 'X' && !$scope.squares[1].val) {
+                aiMoveSet(1);
+              }
+              //Move was blocked, move to next available space
+              else {
+                aiChooseAvailSpaceOther();
               }
             }
           }
@@ -151,8 +202,7 @@ angular.module('ticTacToeApp').controller('MainCtrl', ['$scope', function ($scop
       }
     
     //Called when the AI cannot win, and does not need to block the human player from winning, and just needs to pick another space to move to
-    function aiChooseAvailSpace() {
-        //Cannot do a break in an Angular foreach, so must do it this way.  If game board size changes to large board size, this should be changed to native foreach
+    function aiChooseAvailSpaceMiddleFirst() {
         var proceed = true;
         //Makes the AI stay away from the corners if they are not blocking or trying to win in the next round
         if(!$scope.squares[1].val) {
@@ -171,6 +221,7 @@ angular.module('ticTacToeApp').controller('MainCtrl', ['$scope', function ($scop
             aiMoveSet(7);
             return;
         }
+        //Cannot do a break in an Angular foreach, so must do it this way.  If game board size changes to large board size, this should be changed to native foreach
         angular.forEach($scope.squares, function(item) {
             if(!item.val && proceed) {
               aiMoveSet(item.id);
@@ -178,6 +229,24 @@ angular.module('ticTacToeApp').controller('MainCtrl', ['$scope', function ($scop
             }
           });
       }
+    
+    //Called when the AI cannot win, and does not need to block the human player from winning, and just needs to pick another space to move to
+    function aiChooseAvailSpaceOther() {
+        var proceed = true;
+        //Makes the AI stay away from the corners if they are not blocking or trying to win in the next round
+        if(!$scope.squares[0].val) {
+            aiMoveSet(0);
+            return;
+        }
+        //Cannot do a break in an Angular foreach, so must do it this way.  If game board size changes to large board size, this should be changed to native foreach
+        angular.forEach($scope.squares, function(item) {
+            if(!item.val && proceed) {
+              aiMoveSet(item.id);
+              proceed = false;
+            }
+          });
+      }
+      
 
     //Called to determine if the AI can win with the next move
     function aiCanWin() {
